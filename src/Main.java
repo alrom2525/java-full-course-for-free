5906f43f924b02d4667a32312323199ada145ab9
Alrom <edgardoalbertoromero@gmail.com>
2023-11-29 07:03:17 -0500

    #16 (01:44:54)  2D arrays 🚚

--- src/Main.java
+++ src/Main.java
@@ -9,12 +9,38 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // #15 (01:38:28)  arrays 🚗
+        // #16 (01:44:54)  2D arrays 🚚
+        String[][] cars = new String[3][3];
+        cars[0][0] = "Camaro";
+        cars[0][1] = "Corvette";
+        cars[0][2] = "Silverado";
 
-        String[] cars = {"Camaro", "Corvette", "Tesla", "BMW"};
+        cars[1][0] = "Ford Mustang";
+        cars[1][1] = "Ford Ranger";
+        cars[1][2] = "Ford F-150";
+
+        cars[2][0] = "Ferrari";
+        cars[2][1] = "Lambo";
+        cars[2][2] = "Tesla";
+
+        String[][] cars2 = {
+                            {"Chevrolet Camaro", "Corvette", "Chevrolet Silverado"},
+                            {"Ford Mustang", "Ford Ranger", "Ford F-150"},
+                            {"Ferrari", "Lambo", "Tesla"}
+                    };
+
+        for (int i = 0; i < cars2.length; i++) {
+            System.out.println();
+            for (int j = 0; j < cars2[i].length; j++) {
+                System.out.print(cars2[i][j] + " - ");
+            }
+        }
+        
+        // #15 (01:38:28)  arrays 🚗
+        /* String[] cars = {"Camaro", "Corvette", "Tesla", "BMW"};
         for (int i = 0; i < cars.length ; i++) {
             System.out.println(cars[i]);
-        }
+        }*/
         
         // #14 (01:32:23)  nested loops ➿
         /* Scanner scanner = new Scanner(System.in);
